8b116d9e437e0b4f8c9fe489eebb25fe64deb776
Lena Namiki <lena.namiki@gmail.com>
2021-07-26 11:46:15 +0900

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'about' to: 'pages#about'
-  get 'projects' to: 'pages#projects'
+  get 'about', to: 'pages#about'
+  get 'projects', to: 'pages#projects'
 end
